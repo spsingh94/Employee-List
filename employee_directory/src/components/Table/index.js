0c798d4d2f9632f01471585ad6c294a12792079d
Shaminder Singh <spsingh94.ss@gmail.com>
2020-07-24 15:02:18 -0400

    added jquery

---
+++ employee_directory/src/components/Table/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import $ from "jquery";
+
+function Table() {
+
+    function helloFunc() {
+        $("#hello").click(() => {
+            console.log("hi");
+        });
+        
+    }
+
+return (
+    <table className="table">
+  <thead>
+    <tr>
+      <th id="hello" onClick={helloFunc} scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+);
+};
+
+export default Table;
